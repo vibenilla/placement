883de9ab3b75eeabc76cbf3241382e18e0648db1
mudkipdev <mudkip@mudkip.dev>
2026-02-08 17:13:26 -0700

    add maven central publish workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     java
+    signing
+    id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
+description = "A library for Minestom placement"
 group = "rocks.minestom"
 version = "0.1.0"
 
@@ -9,6 +12,33 @@ java {
     toolchain.languageVersion = JavaLanguageVersion.of(25)
     withSourcesJar()
     withJavadocJar()
+}
+
+mavenPublishing {
+    coordinates(group.toString(), project.name, version.toString())
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        name = project.name
+        description = project.description
+        url = "https://github.com/vibenilla/placement"
+
+        developers {
+            developer {
+                name = "mudkip"
+                id = "mudkipdev"
+                email = "mudkip@mudkip.dev"
+                url = "https://mudkip.dev"
+            }
+        }
+
+        scm {
+            url = "https://github.com/vibenilla/placement"
+            connection = "scm:git:git://github.com/vibenilla/placement.git"
+            developerConnection = "scm:git:ssh://git@github.com/vibenilla/placement.git"
+        }
+    }
 }
 
 repositories {
