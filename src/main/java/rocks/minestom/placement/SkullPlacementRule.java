eec1ba09a269228a223f6fcd85898270a96a3faf
mudkipdev <mudkip@mudkip.dev>
2026-05-13 18:56:40 -0600

    remove incorrect canSurvive from SkullPlacementRule (vanilla skulls float)

--- src/main/java/rocks/minestom/placement/SkullPlacementRule.java
+++ src/main/java/rocks/minestom/placement/SkullPlacementRule.java
@@ -1,7 +1,6 @@
 package rocks.minestom.placement;
 
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.rule.BlockPlacementRule;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,19 +19,5 @@ public final class SkullPlacementRule extends BlockPlacementRule {
         return this.block
                 .withProperty("rotation", Integer.toString(rotation))
                 .withProperty("powered", "false");
-    }
-
-    @Override
-    public Block blockUpdate(UpdateState updateState) {
-
-        if (updateState.fromFace() != BlockFace.BOTTOM) {
-            return updateState.currentBlock();
-        }
-        var below = updateState.instance().getBlock(updateState.blockPosition().relative(BlockFace.BOTTOM));
-
-        if (!below.registry().collisionShape().isFaceFull(BlockFace.TOP)) {
-            return Block.AIR;
-        }
-        return updateState.currentBlock();
     }
 }
