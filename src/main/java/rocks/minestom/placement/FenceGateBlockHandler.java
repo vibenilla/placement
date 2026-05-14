4530d8fef0d1baaaf05111e5f25b194f4f6e8dc9
mudkipdev <mudkip@mudkip.dev>
2026-05-13 18:44:22 -0600

    play fence gate open/close sound per material

--- src/main/java/rocks/minestom/placement/FenceGateBlockHandler.java
+++ src/main/java/rocks/minestom/placement/FenceGateBlockHandler.java
@@ -1,9 +1,14 @@
 package rocks.minestom.placement;
 
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
+import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.BlockHandler;
+import net.minestom.server.sound.SoundEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class FenceGateBlockHandler implements BlockHandler {
     public static final FenceGateBlockHandler INSTANCE = new FenceGateBlockHandler();
@@ -43,9 +48,29 @@ public final class FenceGateBlockHandler implements BlockHandler {
             updatedBlock = updatedBlock.withProperty("open", "true");
         }
 
-        interaction.getInstance().setBlock(interaction.getBlockPosition(), updatedBlock);
+        var instance = interaction.getInstance();
+        var blockPosition = interaction.getBlockPosition();
+        instance.setBlock(blockPosition, updatedBlock);
 
-        // TODO: vanilla plays a per-material fence gate sound; not implemented
+        var pitch = ThreadLocalRandom.current().nextFloat() * 0.1F + 0.9F;
+        var sound = Sound.sound(soundEvent(block, !currentOpen), Sound.Source.BLOCK, 1.0F, pitch);
+        instance.playSound(sound, blockPosition.add(0.5D, 0.5D, 0.5D));
         return false;
+    }
+
+    private static SoundEvent soundEvent(@NotNull Block block, boolean opening) {
+
+        if (block.compare(Block.BAMBOO_FENCE_GATE)) {
+            return opening ? SoundEvent.BLOCK_BAMBOO_WOOD_FENCE_GATE_OPEN : SoundEvent.BLOCK_BAMBOO_WOOD_FENCE_GATE_CLOSE;
+        }
+
+        if (block.compare(Block.CHERRY_FENCE_GATE)) {
+            return opening ? SoundEvent.BLOCK_CHERRY_WOOD_FENCE_GATE_OPEN : SoundEvent.BLOCK_CHERRY_WOOD_FENCE_GATE_CLOSE;
+        }
+
+        if (block.compare(Block.CRIMSON_FENCE_GATE) || block.compare(Block.WARPED_FENCE_GATE)) {
+            return opening ? SoundEvent.BLOCK_NETHER_WOOD_FENCE_GATE_OPEN : SoundEvent.BLOCK_NETHER_WOOD_FENCE_GATE_CLOSE;
+        }
+        return opening ? SoundEvent.BLOCK_FENCE_GATE_OPEN : SoundEvent.BLOCK_FENCE_GATE_CLOSE;
     }
 }
