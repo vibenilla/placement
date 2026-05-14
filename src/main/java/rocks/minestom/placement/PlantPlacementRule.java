04d04043bc78e5cf85ff94e6586de890b935314f
mudkipdev <mudkip@mudkip.dev>
2026-05-13 18:49:02 -0600

    use minecraft:supports_vegetation tag for PlantPlacementRule canSurvive to match vanilla VegetationBlock

--- src/main/java/rocks/minestom/placement/PlantPlacementRule.java
+++ src/main/java/rocks/minestom/placement/PlantPlacementRule.java
@@ -1,32 +1,22 @@
 package rocks.minestom.placement;
 
 import net.kyori.adventure.key.Key;
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.rule.BlockPlacementRule;
 import org.jetbrains.annotations.NotNull;
 
 public final class PlantPlacementRule extends BlockPlacementRule {
+    private static final Key SUPPORTS_VEGETATION_TAG = Key.key("minecraft:supports_vegetation");
+
     public PlantPlacementRule(@NotNull Block block) {
         super(block);
     }
 
     @Override
     public Block blockPlace(@NotNull PlacementState placementState) {
-        var instance = placementState.instance();
-        var supportPosition = placementState.placePosition().relative(BlockFace.BOTTOM);
-        var supportBlock = instance.getBlock(supportPosition);
-        var dirtTag = MinecraftServer.process().blocks().getTag(Key.key("minecraft:dirt"));
-
-        if (dirtTag != null && dirtTag.contains(supportBlock)) {
-            return this.block;
-        }
-
-        if (supportBlock.compare(Block.FARMLAND)) {
-            return this.block;
-        }
-        return null;
+        var below = placementState.instance().getBlock(placementState.placePosition().relative(BlockFace.BOTTOM));
+        return Utility.hasTag(below, SUPPORTS_VEGETATION_TAG) ? this.block : null;
     }
 
     @Override
@@ -36,15 +26,6 @@ public final class PlantPlacementRule extends BlockPlacementRule {
             return updateState.currentBlock();
         }
         var below = updateState.instance().getBlock(updateState.blockPosition().relative(BlockFace.BOTTOM));
-        var dirtTag = MinecraftServer.process().blocks().getTag(Key.key("minecraft:dirt"));
-
-        if (dirtTag != null && dirtTag.contains(below)) {
-            return updateState.currentBlock();
-        }
-
-        if (below.compare(Block.FARMLAND)) {
-            return updateState.currentBlock();
-        }
-        return Block.AIR;
+        return Utility.hasTag(below, SUPPORTS_VEGETATION_TAG) ? updateState.currentBlock() : Block.AIR;
     }
 }
