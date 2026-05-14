84e761042e159c682cac12921e4c8663e7f786fb
mudkipdev <mudkip@mudkip.dev>
2026-05-13 18:44:58 -0600

    play button press and release sounds (wooden 0.6/0.5, stone 0.6/0.5)

--- src/main/java/rocks/minestom/placement/ButtonBlockHandler.java
+++ src/main/java/rocks/minestom/placement/ButtonBlockHandler.java
@@ -1,9 +1,11 @@
 package rocks.minestom.placement;
 
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockHandler;
+import net.minestom.server.sound.SoundEvent;
 import net.minestom.server.timer.TaskSchedule;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,10 +41,14 @@ public final class ButtonBlockHandler implements BlockHandler {
         var instance = interaction.getInstance();
         var blockPosition = interaction.getBlockPosition();
         var pressedBlock = block.withProperty("powered", "true");
+        var wooden = isWooden(block);
 
         instance.setBlock(blockPosition, pressedBlock);
 
-        var delay = this.releaseDelay(block);
+        var pressSound = Sound.sound(wooden ? SoundEvent.BLOCK_WOODEN_BUTTON_CLICK_ON : SoundEvent.BLOCK_STONE_BUTTON_CLICK_ON, Sound.Source.BLOCK, 0.3F, 0.6F);
+        instance.playSound(pressSound, blockPosition.add(0.5D, 0.5D, 0.5D));
+
+        var delay = wooden ? WOODEN_DELAY : STONE_DELAY;
 
         instance.scheduler().buildTask(() -> {
             var currentBlock = instance.getBlock(blockPosition);
@@ -57,20 +63,16 @@ public final class ButtonBlockHandler implements BlockHandler {
             }
 
             instance.setBlock(blockPosition, currentBlock.withProperty("powered", "false"));
+            var releaseSound = Sound.sound(wooden ? SoundEvent.BLOCK_WOODEN_BUTTON_CLICK_OFF : SoundEvent.BLOCK_STONE_BUTTON_CLICK_OFF, Sound.Source.BLOCK, 0.3F, 0.5F);
+            instance.playSound(releaseSound, blockPosition.add(0.5D, 0.5D, 0.5D));
         }).delay(TaskSchedule.tick(delay)).schedule();
 
-        // TODO: vanilla plays the per-material button click sound on press and release
         // TODO: vanilla emits a redstone signal on the connected face; redstone is not simulated here
         return false;
     }
 
-    private int releaseDelay(@NotNull Block block) {
+    private static boolean isWooden(@NotNull Block block) {
         var tag = MinecraftServer.process().blocks().getTag(WOODEN_BUTTONS_TAG);
-
-        if (tag != null && tag.contains(block)) {
-            return WOODEN_DELAY;
-        }
-
-        return STONE_DELAY;
+        return tag != null && tag.contains(block);
     }
 }
