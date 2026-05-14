87bab82bf15e5733eba0eec4fe154f3b5f3dc966
mudkipdev <mudkip@mudkip.dev>
2026-05-13 18:43:56 -0600

    play trapdoor open/close sound per material

--- src/main/java/rocks/minestom/placement/TrapdoorBlockHandler.java
+++ src/main/java/rocks/minestom/placement/TrapdoorBlockHandler.java
@@ -1,9 +1,13 @@
 package rocks.minestom.placement;
 
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockHandler;
+import net.minestom.server.sound.SoundEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class TrapdoorBlockHandler implements BlockHandler {
     public static final TrapdoorBlockHandler INSTANCE = new TrapdoorBlockHandler();
@@ -30,13 +34,41 @@ public final class TrapdoorBlockHandler implements BlockHandler {
             return true;
         }
 
+        var instance = interaction.getInstance();
+        var blockPosition = interaction.getBlockPosition();
         var currentOpen = "true".equals(block.getProperty("open"));
         var newOpen = String.valueOf(!currentOpen);
         var updatedBlock = block.withProperty("open", newOpen);
 
-        interaction.getInstance().setBlock(interaction.getBlockPosition(), updatedBlock);
+        instance.setBlock(blockPosition, updatedBlock);
 
-        // TODO: vanilla plays a per-material trapdoor sound; not implemented
+        var pitch = ThreadLocalRandom.current().nextFloat() * 0.1F + 0.9F;
+        var sound = Sound.sound(soundEvent(block, !currentOpen), Sound.Source.BLOCK, 1.0F, pitch);
+        instance.playSound(sound, blockPosition.add(0.5D, 0.5D, 0.5D));
         return false;
+    }
+
+    private static SoundEvent soundEvent(@NotNull Block block, boolean opening) {
+
+        if (block.compare(Block.IRON_TRAPDOOR)) {
+            return opening ? SoundEvent.BLOCK_IRON_TRAPDOOR_OPEN : SoundEvent.BLOCK_IRON_TRAPDOOR_CLOSE;
+        }
+
+        if (Utility.hasTag(block, Key.key("minecraft:copper_trapdoors"))) {
+            return opening ? SoundEvent.BLOCK_COPPER_TRAPDOOR_OPEN : SoundEvent.BLOCK_COPPER_TRAPDOOR_CLOSE;
+        }
+
+        if (block.compare(Block.BAMBOO_TRAPDOOR)) {
+            return opening ? SoundEvent.BLOCK_BAMBOO_WOOD_TRAPDOOR_OPEN : SoundEvent.BLOCK_BAMBOO_WOOD_TRAPDOOR_CLOSE;
+        }
+
+        if (block.compare(Block.CHERRY_TRAPDOOR)) {
+            return opening ? SoundEvent.BLOCK_CHERRY_WOOD_TRAPDOOR_OPEN : SoundEvent.BLOCK_CHERRY_WOOD_TRAPDOOR_CLOSE;
+        }
+
+        if (block.compare(Block.CRIMSON_TRAPDOOR) || block.compare(Block.WARPED_TRAPDOOR)) {
+            return opening ? SoundEvent.BLOCK_NETHER_WOOD_TRAPDOOR_OPEN : SoundEvent.BLOCK_NETHER_WOOD_TRAPDOOR_CLOSE;
+        }
+        return opening ? SoundEvent.BLOCK_WOODEN_TRAPDOOR_OPEN : SoundEvent.BLOCK_WOODEN_TRAPDOOR_CLOSE;
     }
 }
