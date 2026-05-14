2a4d15534cd0e4ade78b3b41b20197217a7f8f9d
mudkipdev <mudkip@mudkip.dev>
2026-05-13 18:45:49 -0600

    play candle extinguish sound when blowing out a lit candle

--- src/main/java/rocks/minestom/placement/CandleBlockHandler.java
+++ src/main/java/rocks/minestom/placement/CandleBlockHandler.java
@@ -1,8 +1,12 @@
 package rocks.minestom.placement;
 
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import net.minestom.server.instance.block.BlockHandler;
+import net.minestom.server.sound.SoundEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class CandleBlockHandler implements BlockHandler {
     public static final CandleBlockHandler INSTANCE = new CandleBlockHandler();
@@ -26,17 +30,18 @@ public final class CandleBlockHandler implements BlockHandler {
         var block = interaction.getBlock();
         var lit = block.getProperty("lit");
 
-        if ("true".equals(lit)) {
-            var instance = interaction.getInstance();
-            var blockPosition = interaction.getBlockPosition();
-
-            instance.setBlock(blockPosition, block.withProperty("lit", "false"));
-
-            // TODO: vanilla plays an extinguish sound and emits smoke particles
-            return false;
+        if (!"true".equals(lit)) {
+            // TODO: lighting an unlit candle requires flint and steel / fire charge detection on the held item
+            return true;
         }
 
-        // TODO: lighting an unlit candle requires flint and steel / fire charge detection on the held item
-        return true;
+        var instance = interaction.getInstance();
+        var blockPosition = interaction.getBlockPosition();
+        instance.setBlock(blockPosition, block.withProperty("lit", "false"));
+
+        var pitch = ThreadLocalRandom.current().nextFloat() * 0.4F + 0.8F;
+        var sound = Sound.sound(SoundEvent.BLOCK_CANDLE_EXTINGUISH, Sound.Source.BLOCK, 1.0F, pitch);
+        instance.playSound(sound, blockPosition.add(0.5D, 0.5D, 0.5D));
+        return false;
     }
 }
