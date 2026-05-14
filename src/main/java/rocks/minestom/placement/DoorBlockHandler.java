5ce29910896de870a18f727c3b9b093eff2cc77f
mudkipdev <mudkip@mudkip.dev>
2026-05-13 18:43:28 -0600

    play door open/close sound per material (iron, copper, bamboo, cherry, nether wood, wooden)

--- src/main/java/rocks/minestom/placement/DoorBlockHandler.java
+++ src/main/java/rocks/minestom/placement/DoorBlockHandler.java
@@ -1,10 +1,14 @@
 package rocks.minestom.placement;
 
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.BlockHandler;
+import net.minestom.server.sound.SoundEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class DoorBlockHandler implements BlockHandler {
     public static final DoorBlockHandler INSTANCE = new DoorBlockHandler();
@@ -53,7 +57,33 @@ public final class DoorBlockHandler implements BlockHandler {
             instance.setBlock(partnerPosition, partnerBlock.withProperty("open", newOpen));
         }
 
-        // TODO: vanilla plays a per-material door sound; not implemented
+        var pitch = ThreadLocalRandom.current().nextFloat() * 0.1F + 0.9F;
+        var sound = Sound.sound(soundEvent(block, !currentOpen), Sound.Source.BLOCK, 1.0F, pitch);
+        instance.playSound(sound, blockPosition.add(0.5D, 0.5D, 0.5D));
         return false;
+    }
+
+    private static SoundEvent soundEvent(@NotNull Block block, boolean opening) {
+
+        if (block.compare(Block.IRON_DOOR)) {
+            return opening ? SoundEvent.BLOCK_IRON_DOOR_OPEN : SoundEvent.BLOCK_IRON_DOOR_CLOSE;
+        }
+
+        if (Utility.hasTag(block, Key.key("minecraft:copper_doors"))) {
+            return opening ? SoundEvent.BLOCK_COPPER_DOOR_OPEN : SoundEvent.BLOCK_COPPER_DOOR_CLOSE;
+        }
+
+        if (block.compare(Block.BAMBOO_DOOR)) {
+            return opening ? SoundEvent.BLOCK_BAMBOO_WOOD_DOOR_OPEN : SoundEvent.BLOCK_BAMBOO_WOOD_DOOR_CLOSE;
+        }
+
+        if (block.compare(Block.CHERRY_DOOR)) {
+            return opening ? SoundEvent.BLOCK_CHERRY_WOOD_DOOR_OPEN : SoundEvent.BLOCK_CHERRY_WOOD_DOOR_CLOSE;
+        }
+
+        if (block.compare(Block.CRIMSON_DOOR) || block.compare(Block.WARPED_DOOR)) {
+            return opening ? SoundEvent.BLOCK_NETHER_WOOD_DOOR_OPEN : SoundEvent.BLOCK_NETHER_WOOD_DOOR_CLOSE;
+        }
+        return opening ? SoundEvent.BLOCK_WOODEN_DOOR_OPEN : SoundEvent.BLOCK_WOODEN_DOOR_CLOSE;
     }
 }
